51acbb910aaf28862f39bcbdededce5700618602
sandeep <sandeep.gurav@hotmail.com>
2016-07-25 16:21:56 +0530

    test

--- ASPMVC_ShoppingCart/App_Start/RouteConfig.cs
+++ ASPMVC_ShoppingCart/App_Start/RouteConfig.cs
@@ -12,7 +12,7 @@ namespace ASPMVC_ShoppingCart
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
+            //test
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
